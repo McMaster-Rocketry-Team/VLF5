d2e729a0cba6fa4d5b2e85c6b2926721e44981cc
Pegasis <me@pegasis.site>
2025-04-27 13:58:20 -0400

    update embassy

--- firmware/src/main_gcm.rs
+++ firmware/src/main_gcm.rs
@@ -14,12 +14,13 @@ use embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig;
 use embassy_executor::Spawner;
 use embassy_futures::join::join;
 use embassy_stm32::exti::ExtiInput;
-use embassy_stm32::gpio::{Level, Output, Pin, Pull, Speed};
+use embassy_stm32::gpio::{Level, Output, Pull, Speed};
 use embassy_stm32::peripherals::{
     DMA1_CH2, DMA1_CH3, EXTI1, EXTI4, PA2, PA7, PA8, PB3, PB4, PC7, PD0, PD1, PD4, PD5, PD6, SPI3,
 };
 use embassy_stm32::spi::{Config as SpiConfig, Spi};
 use embassy_stm32::time::{mhz, Hertz};
+use embassy_stm32::Peri;
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
 use embassy_time::{Delay, Timer};
 use firmware_common_new::vlp::client::VLPGroundStation;
@@ -108,7 +109,7 @@ async fn main(spawner: Spawner) {
 }
 
 #[embassy_executor::task]
-async fn power_led_task(blue_led: PA2) {
+async fn power_led_task(blue_led: Peri<'static, PA2>) {
     let mut blue_led = Output::new(blue_led, Level::High, Speed::Low);
 
     loop {
@@ -121,21 +122,21 @@ async fn power_led_task(blue_led: PA2) {
 
 #[embassy_executor::task]
 async fn lora_task(
-    green_led: PA7,
-    spi3: SPI3,
-    sck: PB3,
-    mosi: PD6,
-    miso: PB4,
-    cs: PC7,
-    reset: PD5,
-    dio1: PD4,
-    dio1_exti: EXTI4,
-    busy: PD1,
-    busy_exti: EXTI1,
-    txen: PD0,
-    rxen: PA8,
-    tx_dma: DMA1_CH3,
-    rx_dma: DMA1_CH2,
+    green_led: Peri<'static, PA7>,
+    spi3: Peri<'static, SPI3>,
+    sck: Peri<'static, PB3>,
+    mosi: Peri<'static, PD6>,
+    miso: Peri<'static, PB4>,
+    cs: Peri<'static, PC7>,
+    reset: Peri<'static, PD5>,
+    dio1: Peri<'static, PD4>,
+    dio1_exti: Peri<'static, EXTI4>,
+    busy: Peri<'static, PD1>,
+    busy_exti: Peri<'static, EXTI1>,
+    txen: Peri<'static, PD0>,
+    rxen: Peri<'static, PA8>,
+    tx_dma: Peri<'static, DMA1_CH3>,
+    rx_dma: Peri<'static, DMA1_CH2>,
 ) {
     let mut green_led = Output::new(green_led, Level::High, Speed::Low);
 
@@ -153,11 +154,11 @@ async fn lora_task(
         rx_boost: true,
     };
     let iv = GenericSx126xInterfaceVariant::new(
-        Output::new(reset.degrade(), Level::High, Speed::Low),
+        Output::new(reset, Level::High, Speed::Low),
         ExtiInput::new(dio1, dio1_exti, Pull::Down),
         ExtiInput::new(busy, busy_exti, Pull::Down),
-        Some(Output::new(rxen.degrade(), Level::High, Speed::High)),
-        Some(Output::new(txen.degrade(), Level::High, Speed::High)),
+        Some(Output::new(rxen, Level::High, Speed::High)),
+        Some(Output::new(txen, Level::High, Speed::High)),
     )
     .unwrap();
     let sx1262 = Sx126x::new(lora_spi_device, iv, config);
