444150d034d2257392b026cf482daf7408566910
Pegasis <me@pegasis.site>
2025-05-06 10:26:48 -0400

    mag test

---
+++ firmware/src/mag_test.rs
@@ -0,0 +1,115 @@
+// only use std when feature = "std" is enabled or during testing
+#![cfg_attr(not(test), no_std)]
+#![no_main]
+
+mod time;
+mod utils;
+
+use {defmt_rtt as _, panic_probe as _};
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_stm32::i2c::{Config as I2cConfig, I2c};
+use embassy_stm32::time::{mhz, Hertz};
+use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use embassy_time::Timer;
+
+bind_interrupts!(struct Irqs {
+    I2C2_EV => i2c::EventInterruptHandler<peripherals::I2C2>;
+    I2C2_ER => i2c::ErrorInterruptHandler<peripherals::I2C2>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let config = {
+        use embassy_stm32::rcc::mux::*;
+        use embassy_stm32::rcc::*;
+        let mut config = embassy_stm32::Config::default();
+        let rcc = &mut config.rcc;
+
+        rcc.hsi = None;
+        rcc.hse = Some(Hse {
+            freq: mhz(16),
+            mode: HseMode::Oscillator,
+        });
+        rcc.csi = false;
+
+        rcc.hsi48 = None;
+        rcc.sys = Sysclk::PLL1_P;
+
+        rcc.pll1 = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL60,
+            divp: Some(PllDiv::DIV2),
+            divq: Some(PllDiv::DIV20),
+            divr: None,
+        });
+        rcc.pll2 = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL10,
+            divp: Some(PllDiv::DIV4),
+            divq: Some(PllDiv::DIV4),
+            divr: Some(PllDiv::DIV4),
+        });
+        rcc.pll3 = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL10,
+            divp: None,
+            divq: None,
+            divr: Some(PllDiv::DIV8),
+        });
+
+        rcc.d1c_pre = AHBPrescaler::DIV1;
+        rcc.ahb_pre = AHBPrescaler::DIV2;
+        rcc.apb1_pre = APBPrescaler::DIV2;
+        rcc.apb2_pre = APBPrescaler::DIV2;
+        rcc.apb3_pre = APBPrescaler::DIV2;
+        rcc.apb4_pre = APBPrescaler::DIV2;
+
+        rcc.timer_prescaler = TimerPrescaler::DefaultX2;
+        rcc.voltage_scale = VoltageScale::Scale0;
+
+        rcc.ls = LsConfig::default_lsi();
+        rcc.mux.spi123sel = Saisel::PLL2_P;
+        rcc.mux.usart16910sel = Usart16910sel::PLL2_Q;
+        rcc.mux.rngsel = Rngsel::PLL1_Q;
+        rcc.mux.i2c1235sel = I2c1235sel::PLL3_R;
+        rcc.mux.spi45sel = Spi45sel::PLL2_Q;
+        rcc.mux.adcsel = Adcsel::PLL2_P;
+        rcc.mux.usbsel = Usbsel::PLL1_Q;
+        rcc.mux.fdcansel = Fdcansel::PLL2_Q;
+        rcc.mux.sdmmcsel = Sdmmcsel::PLL2_R;
+
+        config
+    };
+    let p = embassy_stm32::init(config);
+    info!("Hello VLF5!");
+    // PS: PA3 (low: force pwm)
+    // red_led: PB1
+    // green_led: PA7
+    // blue_led: PA2
+
+    let mut config = I2cConfig::default();
+    config.sda_pullup = true;
+    config.scl_pullup = true;
+    let mut i2c = I2c::new(
+        p.I2C2,
+        p.PB10,
+        p.PB11,
+        Irqs,
+        p.DMA1_CH7,
+        p.DMA1_CH6,
+        Hertz(100_000),
+        config,
+    );
+
+    let mut data = [0u8; 1];
+    loop {
+        i2c.write_read(0b0011110, &[0x4F], &mut data).await.unwrap();
+        info!("who am i: {}", data[0]);
+        Timer::after_millis(500).await;
+    }
+}
